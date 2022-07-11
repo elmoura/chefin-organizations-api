1afcb1d7056e7f48020323fad7aec0ed5ca017ac
Gabriel Moura <mourasb.gabriel@gmail.com>
2022-07-11 08:01:55 -0300

    feat: add cryptography service

--- src/config/vars.ts
+++ src/config/vars.ts
@@ -3,6 +3,10 @@ import 'dotenv/config';
 export const configuration = {
   appPort: Number(process.env.APP_PORT) || 3000,
   database: {
-    url: process.env.DB_URL,
+    url: process.env.DB_URL || '',
+  },
+  crypto: {
+    secretKey: process.env.CRYPT_SECRET_KEY || '',
+    initializationVector: process.env.CRYPT_IV || '',
   },
 };
